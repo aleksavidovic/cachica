49097b7828f93a4eedce0d3234dad0ec885d3ec4
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-09 07:49:41 +0200

    formatting with ruff

--- src/cachica/server.py
+++ src/cachica/server.py
@@ -3,12 +3,13 @@ import logging
 from asyncio import StreamReader, StreamWriter
 
 logging.basicConfig(
-    level=logging.DEBUG, style='{', format='[{threadName} ({thread})] {message}'
+    level=logging.DEBUG, style="{", format="[{threadName} ({thread})] {message}"
 )
+
 
 async def handle_client(reader: StreamReader, writer: StreamWriter):
     # Get client's address
-    addr = writer.get_extra_info('peername')
+    addr = writer.get_extra_info("peername")
     logging.debug(f"Client connected from: {addr}")
 
     # Read data from the client
@@ -19,9 +20,9 @@ async def handle_client(reader: StreamReader, writer: StreamWriter):
         if not data:
             break
         data_buffer.extend(data)
-    
+
     message = data_buffer.decode()
-    print(f'Received {message} from {addr}')
+    print(f"Received {message} from {addr}")
 
     # Send the response back to the client
     writer.write(data_buffer)
@@ -31,16 +32,16 @@ async def handle_client(reader: StreamReader, writer: StreamWriter):
     print(f"Close the connection with {addr}")
     writer.close()
 
+
 async def run_server():
-    server = await asyncio.start_server(
-        handle_client, '127.0.0.1', 8888
-    )
+    server = await asyncio.start_server(handle_client, "127.0.0.1", 8888)
 
     addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
+    print(f"Serving on {addr}")
 
     async with server:
         await server.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(run_server())
--- tests/unit/test_protocol.py
+++ tests/unit/test_protocol.py
@@ -5,6 +5,7 @@ def test_parse_ping_command():
     resp = parse_command(b"PING\r\n")
     assert resp == ("PING", [])
 
+
 def test_parse_echo_command():
     resp = parse_command(b"ECHO\r\n")
     assert resp == ("ECHO", [])
